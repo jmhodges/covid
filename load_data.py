9b7623652132c512294b6c28c4a1829bf856f71a
Jeff Hodges <jeff@somethingsimilar.com>
2021-12-28 22:26:25 -0800

    total beds attempt (data looks bogus)

--- load_data.py
+++ load_data.py
@@ -27,6 +27,10 @@ def load_hospitalizations():
     df = pandas.read_csv("data/hospitalizations.csv")
     df["time"] = pandas.to_datetime(df["As of Date"])
     df["county"] = df["Facility County"].str.lower()
+    df["Total Staffed Beds"] = df["Total Staffed Beds"].str.replace(
+        ",", "").replace('"', "")
+    df["Total Staffed Beds Currently Available"] = df["Total Staffed Beds Currently Available"].str.replace(
+        ",", "").replace('"', "")
     return (
         df[
             [
@@ -39,6 +43,14 @@ def load_hospitalizations():
                 "Patients Currently in ICU",
                 "Patients Currently ICU Intubated",
                 "Patients Expired",
+                "Total Staffed Beds",
+                "Total Staffed Beds Currently Available",
+                # "Total Staffed ICU Beds",
+                # "Total Staffed ICU Beds Currently Available",
+                # "Total Staffed Acute Care Beds",
+                # "Total Staffed Acute Care Beds Occupied",
+                # "Total Staffed ICU Beds 1",
+                # "Total Staffed ICU Beds Currently Occupied"
             ]
         ]
         .set_index(["time", "county"])
